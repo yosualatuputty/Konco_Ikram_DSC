020fa7831eb202574bc1eb4a231946276d22a08f
Ikram-sabila <ikramsabila@gmail.com>
2024-10-15 10:00:58 +0700

    website

---
+++ Folder Website/routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('main');
+});
+
+Route::get('/main', [App\Http\Controllers\MainController::class, 'index'])->name('dashboard');
+Route::get('/medical-operation', [App\Http\Controllers\MedicalController::class, 'index'])->name('medical.operation');
