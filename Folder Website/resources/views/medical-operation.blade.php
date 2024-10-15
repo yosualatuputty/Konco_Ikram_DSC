2bdb80af27576a26b4f485818f45b492a54b3598
Ikram-sabila <ikramsabila@gmail.com>
2024-10-15 11:05:11 +0700

    computer vision

--- Folder Website/resources/views/medical-operation.blade.php
+++ Folder Website/resources/views/medical-operation.blade.php
@@ -8,6 +8,7 @@
     <h1>Medical Operation</h1>
     <p>Welcome to the Medical Operation page!</p>
     <video id="video" autoplay playsinline></video>
+    <canvas id="canvas" style="display: block; border: 1px solid black;"></canvas>
     <div class="toggle"></div>
     <div class="camera-status" style="display: none;">Camera is off</div>
     <p>This page contains all the information related to medical operations and procedures.</p>
